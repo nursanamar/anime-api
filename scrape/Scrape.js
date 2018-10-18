fdc32c4f478ec6d73811094dcf5be2b396df0b5d
Nursan amar <nursan011@gmail.com>
2018-10-18 16:39:24 +0800

    cookie

--- scrape/Scrape.js
+++ scrape/Scrape.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch')
 module.exports.animeList = (callback = () => {},err = () => {}) => {
     fetch("http://anoboy.org/anime-list-sub-indo/",{
         headers : {
+            "credentials": "same-origin",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
         }
     }).then(res => {
